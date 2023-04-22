1a706b7cc337277759395689a5092e16bcdb9f1c
Casey Mcgrath <caseymcgrath1000@gmail.com>
2023-04-22 16:51:45 -0400

    Add initial html,CSS,JS files

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+// Have start screen with rules & button to start the quiz
+
+// Add arrays of questions & answers
+
+
+// Once start button selected:
+    // Start timer
+    // show first question
+
+// Once answer submitted
+    // Show actual response correct/incorrect 
+    // Update timer
+    // show next question
+
+// Once all questions answered
+    // Show score
+    // Username field, submit
+    // High score screen update
