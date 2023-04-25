f940405ce2584beecdd095fec9c09e8471ce5caf
Casey Mcgrath <caseymcgrath1000@gmail.com>
2023-04-25 00:43:39 -0400

    Built out core functionality of code, including highscore storage & question sequencing

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,14 @@
 const submit = document.querySelector("#submit");
 const highscores = document.querySelector("#highscores");
 const description = document.querySelector("#description");
-const question = document.querySelector("#question")
-const a1 = document.querySelector("#a1");
-const a2 = document.querySelector("#a2");
-const a3 = document.querySelector("#a3");
-const a4 = document.querySelector("#a4");
+const questionText = document.querySelector("#question")
+var questionList = document.querySelector("#questionList");
 var timerEl = document.getElementById('timeleft');
 
 let highScore = localStorage.getItem("highscore");
+let highScoreInitial = localStorage.getItem("highscoreInit")
 let timeLeft = localStorage.getItem("timerCount")
+let questionRef = localStorage.getItem("questionRef")
 
 // Set up questions
 const question1 = {
@@ -24,44 +23,68 @@ const question1 = {
 const question2 = {
     question: "What is the correct answer to question 2?",
     options: ["2a","2b","2c","2d"],
-    correctOption: 1,
+    correctOption: 3,
 }
 
 const questions = [question1, question2];
 
-function renderQuestion(input){
-    question.textContent = input.question;
-    a1.textContent = input.options[0];
-    a2.textContent = input.options[1];
-    a3.textContent = input.options[2];
-    a4.textContent = input.options[3];
-}
+// Quiz Constraints
+var startingTime = 50;
+var timePenalty = 10;
+var ongoing = "no"
 
-function response(input) {
-    if (input === question1.correctOption) {
-        alert("Well done!")
-    }   else {
-        alert("Too Bad")
-        incorrectAnswer()
-
-    }
-}
+// Set up initial page
 
 function showStart(){
+    description.style.display = "block";
     description.textContent = "Welcome to the quiz! Please press Start to begin.";
+    submit.style.display = "block";
     submit.textContent = "Start";
+    question.textContent = "";
+    questionList.innerHTML = "";
+}
+
+// Starting Quiz
+
+function startQuiz(){
+    removeStart();
+    startTimer();
+    questionRef = 0;
+    timeLeft = startingTime;
+    ongoing = "Yes"
+    renderQuestion();
 }
 
 function removeStart(){
-    description.textContent = "";
-    submit.textContent = "";
+    submit.style.display = "none";
+    description.style.display = "none";
+}
+function renderQuestion(){
+    var question = questions[questionRef]
+    questionText.textContent = question.question;
+
+    questionList.innerHTML = "";
+    for (var i = 0; i <question.options.length; i++) {
+        var quest = question.options[i];
+        
+        var li = document.createElement("li");
+        li.textContent = quest;
+        li.setAttribute("data-index",i);
+
+        var button = document.createElement("button");
+        button.textContent = (i+1);
+
+        li.appendChild(button);
+        questionList.appendChild(li);
+    }
 }
 
 function startTimer() {
-    timeLeft = 40;
-
     var timeInterval = setInterval(function () {
-        if (timeLeft > 0) {
+        if (ongoing === "No") {
+            clearInterval(timeInterval);
+        }   else if (timeLeft > 0) {
+            console.log(timeLeft)
             timerEl.textContent = timeLeft;
             timeLeft--;
         }   else {
@@ -71,33 +94,62 @@ function startTimer() {
         }
     },1000);
 }
+// Operating the Quiz
+function response(input) {
+    if (input == questions[questionRef].correctOption) {
+    }   else {
+        incorrectAnswer();
+    }
+
+    if (questionRef === questions.length-1) {
+        let score = timeLeft;
+        endQuiz()
+    }   else {
+        questionRef++;
+        renderQuestion();
+    }
+}
 
 function incorrectAnswer () {
-    timeLeft = timeLeft-5;
+    timeLeft = timeLeft-timePenalty;
 }
 
 function incomplete() {
-    alert('You ran out of time!')
+    alert('You ran out of time!');
+    endQuiz();
 }
 
-function startQuiz(){
-    removeStart();
-    startTimer();
-    renderQuestion(question1);
-    // for (let i=0; i<questions.length;i++){
-    //     renderQuestion(questions[i])
-    //     setTimeout(function() { wonderfulFunction(i) }, 2000);
-    // }
-    // alert("Quiz complete! Thanks for playing.")
+// Ending Quiz & Loading High Scores
+function endQuiz() {
+    addHighScore();
+    showStart();
+    ongoing = "No";
 }
 
-// function showhighscores(){
-//     alert("Show me the high scores!")
-// }
+function addHighScore() {
+    if (highScore === null || timeLeft > highScore) {
+        const initials = prompt("Enter your initials for the new high score!");
+        highScore = timeLeft;
+        highScoreInitial = initials;
+    }
+}
 
+function showHighScores(){
+    alert("The highest score so far is " + highScore + " by " + highScoreInitial)
+}
+
+// Add Listening Events for buttons
 submit.addEventListener("click", startQuiz);
-a1.addEventListener("click", function() { response(0) });
-a2.addEventListener("click", function() { response(1) });
-a3.addEventListener("click", function() { response(2) });
-a4.addEventListener("click", function() { response(3) }); 
-// highscores.addEventListener("click", showhighscores);
+
+highscores.addEventListener("click", showHighScores)
+
+questionList.addEventListener("click", function(event) {
+  var element = event.target;
+
+  if (element.matches("button") === true) {
+    var index = element.parentElement.getAttribute("data-index");
+    response(index)
+  }
+});
+
+showStart()
